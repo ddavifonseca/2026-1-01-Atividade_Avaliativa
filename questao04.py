6dab4b611cb915c9ce2c981d9734565ef2bd1e3d
ddavifonseca <davibarbozadafonseca@gmail.com>
2026-03-27 13:03:32 -0300

    questao04.py
    atualização da parte print, substitui "Você é de Natal" por "Você é de {cidade}"

--- questao04.py
+++ questao04.py
@@ -1,3 +1,3 @@
 nome = input("Nome: ")
 cidade = input("Cidade: ")
-print(f"Bem-vinda(o) a avaliação de ILP, {nome}! Você é de Natal")
+print(f"Bem-vinda(o) a avaliação de ILP, {nome}! Você é de {cidade}")
