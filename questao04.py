2cae8e7e2def4e91000e3e30999e663e7cf6e987
ddavifonseca <davibarbozadafonseca@gmail.com>
2026-03-27 13:04:00 -0300

    realizei as questões

--- questao04.py
+++ questao04.py
@@ -1,3 +1,3 @@
 nome = input("Nome: ")
 cidade = input("Cidade: ")
-print(f"Bem-vinda(o) a avaliação de ILP, {nome}! Você é de {cidade}")
+print(f"Bem-vinda(o) a avaliação de ILP, {nome}! Você é de {cidade}.")
